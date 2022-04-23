d76f2612f6a2f09c87dd739e2e155fd4d1ae6412
Jakeb Williams <Jakebwilliams23@yahoo.com>
2022-04-23 10:43:18 -0500

    Refactored code

--- src/pages/Main/dotloop-Mockup.js
+++ src/pages/Main/dotloop-Mockup.js
@@ -1,62 +1,48 @@
 import React, { Component, useState, useEffect } from "react";
-
 import { PDFDocument } from "pdf-lib";
+import {PdfContainer} from "./styles";
 
-// Created by Jakeb Roos-Williams
+
 // Dotloop Mockup
 // Mocking what dotloop does
 // generates a div per pdf page
 // unlike dotloop each div has an iframe vs a png image of pdf
-export default class DotLoopMockup extends Component {
-    state = {
-      signing: false,
-      pdf: null,
-      list:[],
-      mode: "dotloop",
-      scrollPosition: {},
-      pdfs: []
-  };
+const DotLoopMockup = ({pdf, setPdf}) => {
+  const [frames, setFrames] = useState([])
 
 // add the element to pdf page
-addElementToPage = async(e)=>{
+const addElementToPage = async(e)=>{
   // console.log(`this is the event ${e.clientX} ${e.clientY}`);
-  var list = this.state.list.slice();
+  //var list = this.state.list.slice();
   var preview = document.getElementById("preview");
   // console.log(preview);
-  var element = {type: e.target.id, x: e.clientX, y: e.clientY, page: 0};
-  console.log(`Screen X ${e.screenX} Y ${e.screenY}  Client X ${e.clientX} Y ${e.clientY}`);
-  var newEl = document.createElement("span");
-  newEl.innerHTML = e.target.id;
-  newEl.backgroundColor = "white";
-  newEl.style.position = "absolute";
-  newEl.style.left = element.x+"px";
-  newEl.style.top = element.y+'px';
-  preview.appendChild(newEl);
+  // console.log(`Screen X ${e.screenX} Y ${e.screenY}  Client X ${e.clientX} Y ${e.clientY}`);
+
 
  var bodyRect = document.body.getBoundingClientRect();
  var page = 0;
  while(page < preview.children.length){
-  //  console.log("New Test");
-  // var dpage = preview.children[page];
+   console.log("New Test");
+  var dpage = preview.children[page];
   // var iframe = document.getElementById(dpage.id.replace("page", "frame"));
   // console.log(iframe.getClientRects()[0].height);
   // // DIV 
-  // console.log(`DIV ${page}`);
-  // // div top
-  // console.log(`Div top ${dpage.getClientRects()[0].top}`);
-  // // div left
-  // console.log(`Div left ${dpage.getClientRects()[0].left}`);
-  // // mouse client Y
-  // console.log(`Mouse ${e.clientY}`);
+  console.log(`DIV ${page}`);
+  // div top
+  console.log(`Div top ${dpage.getClientRects()[0].top}`);
+  // div left
+  console.log(`Div left ${dpage.getClientRects()[0].left}`);
+  // mouse client Y
+  console.log(`Mouse ${e.clientY}`);
 
-  // // scrollY
-  // console.log(`Scroll ${window.scrollY}`);
+  // scrollY
+  console.log(`Scroll ${window.scrollY}`);
   // page++;
   page++;
  }
 }
 
-goLookForPage(){
+const goLookForPage = function(){
   // height of document
   console.log(document.documentElement.scrollHeight);
 
@@ -66,28 +52,28 @@ goLookForPage(){
   console.log(window.scrollY);
 }
 
-getPreviewElement(){
+const getPreviewElement = function(){
   var preview = document.getElementById("preview");
   return preview;
 }
 
-getFile = async (event)=>{
+const getFile = async (event)=>{
   const file = event.target.files[0];
   var tmppath = URL.createObjectURL(file);
 }
 
 // goes finds the page from x,y event coordinates
-calculatePage= async(e)=>{
+/* const calculatePage= async(e)=>{
   this.state.pdfs.forEach(pdf => {
     var frame = document.getElementById(pdf.id);
     console.log(frame);
     console.log(e);
   });
   return 0;
-}
+} */
 
 // reads file and generate divs for each page of the pdf 
- readFileToGenerate = (event) => { // event parameter = e: event
+ const readFileToGenerate = (event) => { // event parameter = e: event
     // read pdf
     // create newFileReader
     const reader = new FileReader();
@@ -103,19 +89,19 @@ calculatePage= async(e)=>{
     reader.onloadend = async() => {
       pdf2 = reader.result;
       // get pages
-      pdfDoc2 = await PDFDocument.load(pdf2);
-      pages = pdfDoc2.getPages();
-      this.generateDivs(pages, pdfDoc2);
-      this.setState({pdf: pdf2, pdfs: []});
+      //pdfDoc2 = await PDFDocument.load(pdf2);
+      //pages = pdfDoc2.getPages();
+      //generateDivs(pdf2);
+      setPdf(pdf2);
     };
-    window.addEventListener("scroll", ()=>{
+/*     window.addEventListener("scroll", ()=>{
       this.setState({scrollY: window.scrollY});
-    }); 
+    });  */
     
     reader.readAsDataURL(file);
    }
 
-   generateImages(){
+   const generateImages = function(){
    }
 
   // generate DIVs for each page of pdf like dotloop
@@ -123,7 +109,7 @@ calculatePage= async(e)=>{
 // testing dotloop, reading sourcecode of dotloop, looking at our source,
 // comparing and contrasting source to programmatically create a mockup like dotloop
 // generating a div per pdf page
-  generateDivs = async(pdfPages, srcpdf=null)=>{
+  const generateDivs = async(pdfPages, srcpdf=null)=>{
     var i = 0;
     var pdfs = [];
     while(i < pdfPages.length){
@@ -136,50 +122,51 @@ calculatePage= async(e)=>{
       pdfNew = null;
       i++;
     }
-    this.createFrames(pdfs);
+    createFrames(pdfs);
 }
 
-createFrames(pdfs){
+const createFrames = function(pdfs){
   var documentEditor = this.getPreviewElement();
   documentEditor.innerHTML = "";
-  var framecount = 0;
-  pdfs.forEach(pdf => {
-    var div = document.createElement('div');
-    div.id = "document-page-"+framecount;
-    div.innerHTML = `<iframe id='document-frame-${framecount}' width='100%' height='100%' src=${pdf}></iframe>`;
-    div.style.borderColor = "black";
-    div.style.borderStyle = "solid";
-    div.style.borderWidth = "1px";
-    div.style.marginBottom = "20px";
-    div.style.height = "600px";
-    documentEditor.appendChild(div);
-    framecount++;
-  });
+
+  const frames = pdfs.map(toFrame)
+
+  setFrames(frames)
+}
+
+const toFrame = (pdf, index) => {
+  return <div id={`document-page-${index}`}>
+  <iframe 
+  id={`document-frame-${index}`}
+   width='100%' 
+   height='100%' 
+  style={{border: '1px solid black', marginBottom: 20, height: 600}}
+  src={pdf}></iframe>
+</div>
 }
 
 
-componentWillUnmount(){
+/* componentWillUnmount(){
   this.setState({});
-}
+} */
 
 
 
-render(){
-        return (<div>
+    return (<div>
           <h2>Dotloop Mockup</h2>
         <div className="form-group">
           <input    
-          type="file" onChange={this.readFileToGenerate}/>
+          type="file" onChange={readFileToGenerate}/>
           {/* 
               readFileToGenerate function is to generate multiple divs per pdfpage inside preview based on pdfdocument like dotloop line 270
               handleChange function is the initial function for previewing the whole pdf inside the iframe pdframe line 273
           */}
         </div>
-        <div id="preview">
+          <PdfContainer><iframe id="pdframe" title="pdframe" src={pdf}></iframe></PdfContainer>
+          {/*{frames.map(frame => frame)}*/}
         </div> 
-    </div>);
+    );
 }
-}
 
 
-
+export default DotLoopMockup
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-
-import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
-
+import React, {  useState, useRef } from "react";
 import fontkit from "@pdf-lib/fontkit";
 import SignatureCanvas from "react-signature-canvas";
 import DotLoopMockup from "./dotloop-Mockup";
+import Initial from "./initial";
+import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
+
 
 import {
   FaPencilAlt,
@@ -17,34 +17,38 @@ import {
 import { SignContainer, PdfContainer, SignButton } from "./styles";
 import Container from "../../components/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-export default class Main extends DotLoopMockup {
+const Main = () => {
+
+  const [mode, setMode] = useState("initial")
+  const [signing, setSigning] = useState(false)
+  const [pdf, setPdf] = useState([])
+  const [pdfs, setPdfs] = useState([])
 
   //intial value of textField counter
-  i=0;
+  //var i=0;
 
-  sigPad = {};
+  const sigPad = useRef(null);
 
-  clear = () => {
-    this.sigPad.clear();
+  const clear = () => {
+    sigPad.clear();
   };
 
-  trim = async () => {
-    const { pdf } = this.state;
+  const trim = async () => {
 
     this.setState({ signing: true });
     const url = "https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf";
 
-    const trimmedDataURL = this.sigPad
+    const trimmedDataURL = sigPad
       .getTrimmedCanvas()
       .toDataURL("image/png");
 
     if (pdf) {
-      this.pdfDoc = await PDFDocument.load(pdf);
+      const pdfDoc = await PDFDocument.load(pdf);
 
-      const pngImage = await this.pdfDoc.embedPng(trimmedDataURL);
+      const pngImage = await pdfDoc.embedPng(trimmedDataURL);
       const pngDims = pngImage.scale(0.17);
 
-      const pages = this.pdfDoc.getPages();
+      const pages = pdfDoc.getPages();
       const firstPage = pages[0];
 
       firstPage.drawImage(pngImage, {
@@ -65,7 +69,7 @@ export default class Main extends DotLoopMockup {
         second: "2-digit",
       }).format(timestamp);
 
-      const helveticaFont = await this.pdfDoc.embedFont(StandardFonts.Helvetica);
+      const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
       firstPage.drawText("Digital Signature Verified on " + ts, {
         x: 460,
@@ -75,129 +79,46 @@ export default class Main extends DotLoopMockup {
         color: rgb(0.95, 0.1, 0.1),
       });
 
-      const pdfBytes = await this.pdfDoc.saveAsBase64({ dataUri: true });
+      const pdfBytes = await pdfDoc.saveAsBase64({ dataUri: true });
 
       // await this.sleep(300);
-      this.setState({ pdf: pdfBytes, signing: false });
+      setPdf(pdfBytes)
+      setSigning(false)
     } else {
-      this.setState({ signing: false });
+      setSigning(false);
     }
   };
 
-  handleChange = (e) => {
-    const reader = new FileReader();
-    const file = e.target.files[0];
-
-    reader.onloadend = () => {
-      this.setState({ pdf: reader.result });
-    };
-
-    reader.readAsDataURL(file);
-  };
-
-
-  addToPage = async (event) =>{
-    // creates a mutable version of the array since it's immutable
-     var newArray = this.state.list.slice(); 
-    // creates entry variable
-     var entry = {type: event.target.id, x: event.screenX, y: event.screenY, page: null}; 
-    
-     // pushes the entry to newArray
-     newArray.push(entry);
-     // sets the new state for list
-     this.setState({list: newArray});
-      var checkbox;
-      var sig;
-      var radio;
-      var text;
-    
-    this.i++;
-
-    const frame = document.getElementById("pdframe");
-    //  const dragBtn = document.getElementById("dragMeBtn");
-    //  // boundaries for frame
-    //  const frameBoundaries = frame.getBoundingClientRect();
-    //  console.log(frameBoundaries);
-    const pdfDoc1 = await PDFDocument.load(frame.src);
-    const form = pdfDoc1.getForm();
-    const pages = pdfDoc1.getPages();
-      if(event.target.id=="Signature"){
-          
-      }
   
-      if(event.target.id=="Text"){
-        text = form.createTextField(`text`)
-        text.addToPage(pages[0], {
-          x: 0,
-          y: 0
-        });
-      }
-  
-      if(event.target.id=="Checkbox"){
-        checkbox = form.createCheckBox(`checkbox1`);
-        checkbox.addToPage(pages[0], {
-          x:0,
-          y:50});
-      }
-  
-      if(event.target.id=="RadioBtn"){
-        radio = form.createRadioGroup(`radio`);
-        radio.addOptionToPage('radioBtn1', pages[0], {y:100, x:0} );
-      }
-  
-  
-    const pdfBytes1 = await pdfDoc1.saveAsBase64({ dataUri: true });
-    this.setState({pdf: pdfBytes1, list: newArray});
-   }
 
- ShowList(){
+
+ const ShowList = () => {
   // console.log("This is the new list");
  }
   
-  render() {
-    const { signing, pdf } = this.state;
     var loading = false;
-    const { x, y } = this.state;
+   // const { x, y } = this.state;
+
     return (
       <Container>
         <div>
           Mode: 
-            <button onClick={()=> this.setState({mode: 'initial'})}>initial</button>
-            <button onClick={()=> this.setState({mode: 'dotloop'})}>dotloop</button>
+            <button onClick={()=> setMode('initial')}>initial</button>
+            <button onClick={()=> setMode('dotloop')}>dotloop</button>
           <br/>
-          <div className="nav-btns">
-                  <button id="Signature" style={{height: '30px', width : '100px'}} draggable="true" onDragEnd={this.state.mode === "initial" ? this.addToPage : this.addElementToPage}>Signature</button> 
-                  <button id="Text" style={{height: '30px', width : '100px'}} draggable="true" onDragEnd={this.state.mode === "initial" ? this.addToPage : this.addElementToPage}>Text</button> 
-                  <button id="Checkbox" style={{height: '30px', width : '100px'}} draggable="true" onDragEnd={this.state.mode === "initial" ? this.addToPage : this.addElementToPage}>Checkbox</button> 
-                  <button id="RadioBtn" style={{height: '30px', width : '100px'}} draggable="true" onDragEnd={this.state.mode === "initial" ? this.addToPage : this.addElementToPage}>Radio Button</button> 
-          </div>
         </div>
-        <div style={{ display: this.state.mode !=="initial" ? "block":"none"}}>
-          <DotLoopMockup></DotLoopMockup>
-        </div>
+        {mode === "initial" ? <Initial setPdf={setPdf} pdf={pdf} /> : <DotLoopMockup setPdf={setPdf} pdf={pdf}></DotLoopMockup>}
         {/* <h1> */}
-        <h1 style={{visibility: this.state.mode!=="dotloop" ? "visible":"hidden"}}>
-        <FaFileSignature />
-        <a href="/"> digital-signature</a>
-        <input type="file" onChange={this.handleChange} />
-        </h1>
-
-        <PdfContainer style={{display: this.state.mode!=="dotloop" ? "block":"none"}}>
-          <iframe id="pdframe" title="pdframe" src={pdf}></iframe>
-        </PdfContainer> 
-
         <SignContainer>
           <SignatureCanvas
             penColor="black"
-            ref={(ref) => {
-              this.sigPad = ref;
-            }}
+            ref={sigPad}
           />
           <div>
-            <button type="button" onClick={this.clear} disabled={signing}>
+            <button type="button" onClick={clear} disabled={signing}>
               <FaEraser color="#fff" size={14} />
             </button>  
-            <SignButton onClick={this.trim} disabled={signing}>
+            <SignButton onClick={trim} disabled={signing}>
               {signing ? (
                 <FaSpinner color="#fff" size={14} />
               ) : (
@@ -213,5 +134,6 @@ export default class Main extends DotLoopMockup {
         </SignContainer>
       </Container>
     );
-  }
 }
+
+export default Main
